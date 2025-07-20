88d16c24f1ae88cf3a6eadaa76c76b904fc2e335
Benjamin Beyssac <benjamin.beyssac@gmail.com>
2025-07-20 03:03:15 +0200

    update css

--- blocks/crossroad-pages/index.js
+++ blocks/crossroad-pages/index.js
@@ -5,7 +5,7 @@
  * WordPress link search popover allowing the user to pick a page. Selected
  * pages are shown as small previews.
  */
-( function( blocks, element, data, components, blockEditor ) {
+(function (blocks, element, data, components, blockEditor) {
     var el = element.createElement;
     var useState = element.useState;
     var useSelect = data.useSelect;
@@ -13,29 +13,29 @@
     var Popover = components.Popover;
     var LinkControl = blockEditor.LinkControl || blockEditor.__experimentalLinkControl;
 
-    function PagePreview( props ) {
+    function PagePreview(props) {
         var id = props.id;
-        var record = useSelect( function( select ) {
-            var page = select( 'core' ).getEntityRecord( 'postType', 'page', id );
-            var media = page && page.featured_media ? select( 'core' ).getMedia( page.featured_media ) : null;
+        var record = useSelect(function (select) {
+            var page = select('core').getEntityRecord('postType', 'page', id);
+            var media = page && page.featured_media ? select('core').getMedia(page.featured_media) : null;
             return { page: page, media: media };
-        }, [ id ] );
+        }, [id]);
 
-        if ( ! record.page ) {
-            return el( 'div', { className: 'crossroad-page loading', key: id }, '...' );
+        if (!record.page) {
+            return el('div', { className: 'crossroad-page loading', key: id }, '...');
         }
 
         var style = {};
-        if ( record.media && record.media.source_url ) {
+        if (record.media && record.media.source_url) {
             style.backgroundImage = 'url(' + record.media.source_url + ')';
         }
 
-        return el( 'div', { className: 'crossroad-page', style: style, key: id },
-            el( 'span', { className: 'crossroad-page-title' }, record.page.title.rendered )
+        return el('div', { className: 'crossroad-page', style: style, key: id },
+            el('span', { className: 'crossroad-page-title' }, record.page.title.rendered)
         );
     }
 
-    blocks.registerBlockType( 'twentytwentyfive-child/crossroad-pages', {
+    blocks.registerBlockType('twentytwentyfive-child/crossroad-pages', {
         title: 'Crossroad Pages',
         icon: 'grid-view',
         category: 'widgets',
@@ -46,45 +46,45 @@
                 default: []
             }
         },
-        edit: function( props ) {
+        edit: function (props) {
             var pages = props.attributes.pages || [];
             var setAttributes = props.setAttributes;
 
-            var _useState = useState( false ),
+            var _useState = useState(false),
                 isOpen = _useState[0],
                 setOpen = _useState[1];
 
-            function addPage( value ) {
-                if ( value && value.id ) {
-                    setAttributes( { pages: pages.concat( value.id ) } );
+            function addPage(value) {
+                if (value && value.id) {
+                    setAttributes({ pages: pages.concat(value.id) });
                 }
-                setOpen( false );
+                setOpen(false);
             }
 
-            return el( 'div', {},
-                el( 'div', { className: 'crossroad-pages' },
-                    pages.map( function( id ) {
-                        return el( PagePreview, { id: id, key: id } );
-                    } ),
-                    el( Button, {
+            return el('div', { className: 'crossroad-pages-container' },
+                el('div', { className: 'crossroad-pages' },
+                    pages.map(function (id) {
+                        return el(PagePreview, { id: id, key: id });
+                    }),
+                    el(Button, {
                         icon: 'plus',
-                        onClick: function() { setOpen( true ); },
+                        onClick: function () { setOpen(true); },
                         variant: 'primary'
-                    } )
+                    })
                 ),
-                isOpen && el( Popover, { position: 'bottom', onClose: function() { setOpen( false ); } },
-                    el( LinkControl, {
+                isOpen && el(Popover, { position: 'top', onClose: function () { setOpen(false); } },
+                    el(LinkControl, {
                         onChange: addPage,
                         searchInputPlaceholder: 'Rechercher une page',
                         kind: 'post-type',
                         type: 'page'
-                    } )
+                    })
                 )
             );
         },
-        save: function() { return null; }
-    } );
-} )(
+        save: function () { return null; }
+    });
+})(
     window.wp.blocks,
     window.wp.element,
     window.wp.data,
